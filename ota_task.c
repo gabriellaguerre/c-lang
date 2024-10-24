9bc4cdda4520e3b34e94b2b451dafee2f4f6a18a
gabriel <gabriel.g.laguerre@gmail.com>
2024-10-24 10:33:02 -0400

    removing ota services

--- ota_task.c
+++ ota_task.c
@@ -135,54 +135,54 @@ void * otaTask(void *pvParameter)
 
 ota_task_restart:
     /* filling the TCP server socket address */
-    sLocalAddr.sin_family = SL_AF_INET;
-    sLocalAddr.sin_port = sl_Htons((uint16_t)OTA_REPORT_SERVER_PORT);
-    sLocalAddr.sin_addr.s_addr = SL_INADDR_ANY;
+//     sLocalAddr.sin_family = SL_AF_INET;
+//     sLocalAddr.sin_port = sl_Htons((uint16_t)OTA_REPORT_SERVER_PORT);
+//     sLocalAddr.sin_addr.s_addr = SL_INADDR_ANY;
 
-    addrSize = sizeof(SlSockAddrIn_t);
-    UART_PRINT("[ota report task] Attempting to open OTA socket on port %d...\n", OTA_REPORT_SERVER_PORT);
-    sock = sl_Socket(sLocalAddr.sin_family, SL_SOCK_STREAM, 0);
+//     addrSize = sizeof(SlSockAddrIn_t);
+//     UART_PRINT("[ota report task] Attempting to open OTA socket on port %d...\n", OTA_REPORT_SERVER_PORT);
+//     sock = sl_Socket(sLocalAddr.sin_family, SL_SOCK_STREAM, 0);
 
-    if(sock < 0)
-    {
-        UART_PRINT("[ota report task] Error openning socket, %d \n\r", sock);
-//        sl_Close(sock);
-        usleep(2000000);  // Sleep for 1 second before retrying
-        goto ota_task_restart;
-    }
-    UART_PRINT("[ota report task] OTA socket opened successfully, attempting to bind...\n");
-    status = sl_Bind(sock, (SlSockAddr_t *)&sLocalAddr, addrSize);
-    if(status < 0)
-    {
-        UART_PRINT("[ota report task] Error binding socket, error %d \n\r",
-                   status);
-        sl_Close(sock);
-        usleep(1000000);  // Sleep for 1 second before retrying
-        goto ota_task_restart;
-    }
-    UART_PRINT("[ota report task] OTA socket bound successfully, attempting to listen...\n");
-    status = sl_Listen(sock, 0);
-    if(status < 0)
-    {
-        UART_PRINT("[ota report task] Error listening on socket, error %d \n\r",status);
-        UART2_write(uartHandle, "[ota report task] Error listening on socket, error", strlen("[ota report task] Error listening on socket, error"), NULL);
-        sl_Close(sock);
-        goto ota_task_restart;
-    }
+//     if(sock < 0)
+//     {
+//         UART_PRINT("[ota report task] Error openning socket, %d \n\r", sock);
+// //        sl_Close(sock);
+//         usleep(2000000);  // Sleep for 1 second before retrying
+//         goto ota_task_restart;
+//     }
+//     UART_PRINT("[ota report task] OTA socket opened successfully, attempting to bind...\n");
+//     status = sl_Bind(sock, (SlSockAddr_t *)&sLocalAddr, addrSize);
+//     if(status < 0)
+//     {
+//         UART_PRINT("[ota report task] Error binding socket, error %d \n\r",
+//                    status);
+//         sl_Close(sock);
+//         usleep(1000000);  // Sleep for 1 second before retrying
+//         goto ota_task_restart;
+//     }
+//     UART_PRINT("[ota report task] OTA socket bound successfully, attempting to listen...\n");
+//     status = sl_Listen(sock, 0);
+//     if(status < 0)
+//     {
+//         UART_PRINT("[ota report task] Error listening on socket, error %d \n\r",status);
+//         UART2_write(uartHandle, "[ota report task] Error listening on socket, error", strlen("[ota report task] Error listening on socket, error"), NULL);
+//         sl_Close(sock);
+//         goto ota_task_restart;
+//     }
 
-    nonBlocking = 1;
-    UART_PRINT("[ota report task] Attempting to set OTA socket as non-blocking...\n");
-    status = sl_SetSockOpt(sock, SL_SOL_SOCKET, SL_SO_NONBLOCKING, &nonBlocking,
-                           sizeof(nonBlocking));
-    if(status < 0)
-    {
-        UART_PRINT(
-            "[ota report task] Error setting socket as non-blocking, "
-            "error %d \n\r",
-            status);
-        sl_Close(sock);
-        goto ota_task_restart;
-    }
+//     nonBlocking = 1;
+//     UART_PRINT("[ota report task] Attempting to set OTA socket as non-blocking...\n");
+//     status = sl_SetSockOpt(sock, SL_SOL_SOCKET, SL_SO_NONBLOCKING, &nonBlocking,
+//                            sizeof(nonBlocking));
+//     if(status < 0)
+//     {
+//         UART_PRINT(
+//             "[ota report task] Error setting socket as non-blocking, "
+//             "error %d \n\r",
+//             status);
+//         sl_Close(sock);
+//         goto ota_task_restart;
+//     }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++ RS485 server setup starts ++++++++++++++++++++++++++++++++++++++++
     rs485Addr.sin_family = SL_AF_INET;
@@ -231,63 +231,63 @@ ota_task_restart:
         mailboxTrials = 0;
 
  /* waits for ota request from client before openning the ota response server */
-        sem_wait(&LinkLocal_ControlBlock.otaReportServerStartSignal);
+        // sem_wait(&LinkLocal_ControlBlock.otaReportServerStartSignal);
 
-        while((otaProgressBar != 100) && (otaProgressBar != 0xFF))
-        {
-ota_task_accept_start:
-            newsock = SL_ERROR_BSD_EAGAIN;
-            rs485NewSock = SL_ERROR_BSD_EAGAIN;    // +++++ RS485 socket accept start +++++
+//         while((otaProgressBar != 100) && (otaProgressBar != 0xFF))
+//         {
+// ota_task_accept_start:
+//             newsock = SL_ERROR_BSD_EAGAIN;
+//             rs485NewSock = SL_ERROR_BSD_EAGAIN;    // +++++ RS485 socket accept start +++++
 
-            /* wait for client */
-            while(newsock < 0)
-            {
-                newsock =
-                    sl_Accept(sock, ( struct SlSockAddr_t *)&sAddr,
-                              (SlSocklen_t *)&addrSize);
-                if((newsock == SL_ERROR_BSD_EAGAIN) && nonBlocking)
-                {
-                    usleep(OTA_NB_TIMEOUT * 1000);
+//             /* wait for client */
+//             while(newsock < 0)
+//             {
+//                 newsock =
+//                     sl_Accept(sock, ( struct SlSockAddr_t *)&sAddr,
+//                               (SlSocklen_t *)&addrSize);
+//                 if((newsock == SL_ERROR_BSD_EAGAIN) && nonBlocking)
+//                 {
+//                     usleep(OTA_NB_TIMEOUT * 1000);
 
-                    /* increase accept counter to protect from cases were
-                    connection cannot be created so OTA task would not block
-                    the procedure */
-                    acceptTrials++;
-                    if(acceptTrials > OTA_NUM_OF_ACCEPT_TRIALS)
-                    {
-                        UART_PRINT(
-                            "[ota report task] Error accepting client "
-                            "connection, aborting progress bar... \n\r");
-                        acceptTrials = 0;
+//                     /* increase accept counter to protect from cases were
+//                     connection cannot be created so OTA task would not block
+//                     the procedure */
+//                     acceptTrials++;
+//                     if(acceptTrials > OTA_NUM_OF_ACCEPT_TRIALS)
+//                     {
+//                         UART_PRINT(
+//                             "[ota report task] Error accepting client "
+//                             "connection, aborting progress bar... \n\r");
+//                         acceptTrials = 0;
 
-                        goto ota_task_end;
-                    }
-                }
-                else if(newsock < 0)
-                {
-                    UART_PRINT(
-                        "[ota report task] Error accepting client connection,"
-                        "error %d \n\r",
-                        newsock);
-                    /* means client failed to connect and command to driver
-                     has aborted */
-                   /* in this case, it is better to abort the report server
-                    and continue */
-                   /* with file upload to server */
-                    if((newsock == SL_RET_CODE_DEV_LOCKED) ||(newsock == SL_API_ABORTED))
-                    {
-                        goto ota_task_end;
-                    }
-                    else
-                    {
-                    goto ota_task_accept_start;
-                    }
-                }
-                else
-                {
-                    acceptTrials = 0;
-                }
-            }
+//                         goto ota_task_end;
+//                     }
+//                 }
+//                 else if(newsock < 0)
+//                 {
+//                     UART_PRINT(
+//                         "[ota report task] Error accepting client connection,"
+//                         "error %d \n\r",
+//                         newsock);
+//                     /* means client failed to connect and command to driver
+//                      has aborted */
+//                    /* in this case, it is better to abort the report server
+//                     and continue */
+//                    /* with file upload to server */
+//                     if((newsock == SL_RET_CODE_DEV_LOCKED) ||(newsock == SL_API_ABORTED))
+//                     {
+//                         goto ota_task_end;
+//                     }
+//                     else
+//                     {
+//                     goto ota_task_accept_start;
+//                     }
+//                 }
+//                 else
+//                 {
+//                     acceptTrials = 0;
+//                 }
+//             }
 
 // ++++++++++++++++++++++++++++++++++++ RS485 client accept starts ++++++++++++++++++++++++++++++++++++++++++++++++++
             while(rs485NewSock < 0)
@@ -387,112 +387,112 @@ ota_task_accept_start:
             }
 
             /* flush the mailbox since post may be faster than fetch */
-            mailboxItems = 0;
-            mailboxTrials = 0;
-            while(mailboxItems == 0)
-            {
-                do
-                {
-                    clock_gettime(CLOCK_REALTIME, &ts);
-                    ts.tv_nsec += (1000000 * OTA_REPORT_TIMEOUT);
-                    ts.tv_sec += (ts.tv_nsec / 1000000000);
-                    ts.tv_nsec = ts.tv_nsec % 1000000000;
+            // mailboxItems = 0;
+            // mailboxTrials = 0;
+            // while(mailboxItems == 0)
+            // {
+            //     do
+            //     {
+            //         clock_gettime(CLOCK_REALTIME, &ts);
+            //         ts.tv_nsec += (1000000 * OTA_REPORT_TIMEOUT);
+            //         ts.tv_sec += (ts.tv_nsec / 1000000000);
+            //         ts.tv_nsec = ts.tv_nsec % 1000000000;
 
-                    msgqRetVal = mq_timedreceive(
-                        LinkLocal_ControlBlock.reportServerMQueue,
-                        (char *)&otaProgressBar, sizeof(uint8_t), NULL, &ts);
-                    if(msgqRetVal >= 0)
-                    {
-                        mailboxItems++;
-                    }
-                }
-                while(msgqRetVal >= 0);
+            //         msgqRetVal = mq_timedreceive(
+            //             LinkLocal_ControlBlock.reportServerMQueue,
+            //             (char *)&otaProgressBar, sizeof(uint8_t), NULL, &ts);
+            //         if(msgqRetVal >= 0)
+            //         {
+            //             mailboxItems++;
+            //         }
+            //     }
+            //     while(msgqRetVal >= 0);
 
-                mailboxTrials++;
-                if(mailboxTrials > OTA_NUM_OF_MAILBOX_TRIALS)
-                {
-                    UART_PRINT("[ota report task] Error reading from mailbox, aborting progress bar... \n\r");
-                    sl_Close(newsock);
-                    mailboxTrials = 0;
+            //     mailboxTrials++;
+            //     if(mailboxTrials > OTA_NUM_OF_MAILBOX_TRIALS)
+            //     {
+            //         UART_PRINT("[ota report task] Error reading from mailbox, aborting progress bar... \n\r");
+            //         sl_Close(newsock);
+            //         mailboxTrials = 0;
 
-                    goto ota_task_end;
-                }
-            }
+            //         goto ota_task_end;
+            //     }
+            // }
 
-            if(otaProgressBar == 0xFF)
-            {
-                UART_PRINT(
-                    "[ota report task] OTA progress failed, aborting... \n\r");
-                strcpy((char *)gPayloadData, "fail");
-                contentLen = strlen((const char *)gPayloadData);
-            }
-            else
-            {
-                UART_PRINT("[ota report task] OTA progress %d%% \n\r",
-                           otaProgressBar);
-                snprintf((char *)gPayloadData, OTA_PROGRESS_BAR_STR_LEN, "%d",
-                         otaProgressBar);
-                contentLen = strlen((const char *)gPayloadData);
-            }
+            // if(otaProgressBar == 0xFF)
+            // {
+            //     UART_PRINT(
+            //         "[ota report task] OTA progress failed, aborting... \n\r");
+            //     strcpy((char *)gPayloadData, "fail");
+            //     contentLen = strlen((const char *)gPayloadData);
+            // }
+            // else
+            // {
+            //     UART_PRINT("[ota report task] OTA progress %d%% \n\r",
+            //                otaProgressBar);
+            //     snprintf((char *)gPayloadData, OTA_PROGRESS_BAR_STR_LEN, "%d",
+            //              otaProgressBar);
+            //     contentLen = strlen((const char *)gPayloadData);
+            // }
 
-            snprintf(
-                (char *)gMetadataResponse, HTTP_HEADER_METADATA_STR_LEN,
-                "HTTP/1.0 200 OK\r\nContent-type: "
-                "text/html\r\nAccess-Control-Allow-Origin: *\r\nContent-Length:"
-                "%d\r\n\r\n",
-                (int)contentLen);
-            while(1)
-            {
-                status =
-                    sl_Send(newsock, gMetadataResponse,
-                            strlen((const char *)gMetadataResponse), 0);
-                if((status == SL_ERROR_BSD_EAGAIN) && nonBlocking)
-                {
-                    INFO_PRINT("[ota report task] pending metadata send \n\r");
-                    usleep(OTA_NB_TIMEOUT * 1000);
-                    continue;
-                }
-                else if(status < 0)
-                {
-                    UART_PRINT(
-                        "[ota report task] Error sending progress metadata to "
-                        "client, error %d \n\r",
-                        status);
-                    sl_Close(newsock);
-                    goto ota_task_accept_start;
-                }
-                else
-                {
-                   INFO_PRINT("[ota report task] sent metadata to client \n\r");
-                    break;
-                }
-            }
-            while(1)
-            {
-                status = sl_Send(newsock, gPayloadData, contentLen, 0);
-                if((status == SL_ERROR_BSD_EAGAIN) && nonBlocking)
-                {
-                    INFO_PRINT("[ota report task] pending report send \n\r");
-                    usleep(OTA_NB_TIMEOUT * 1000);
-                    continue;
-                }
-                else if(status < 0)
-                {
-                    UART_PRINT(
-                        "[ota report task] Error sending progress data to "
-                        "client, error %d \n\r",
-                        status);
-                    sl_Close(newsock);
-                    goto ota_task_accept_start;
-                }
-                else
-                {
-                    INFO_PRINT("[ota report task] sent payload to client \n\r");
-                    break;
-                }
-            }
+            // snprintf(
+            //     (char *)gMetadataResponse, HTTP_HEADER_METADATA_STR_LEN,
+            //     "HTTP/1.0 200 OK\r\nContent-type: "
+            //     "text/html\r\nAccess-Control-Allow-Origin: *\r\nContent-Length:"
+            //     "%d\r\n\r\n",
+            //     (int)contentLen);
+            // while(1)
+            // {
+            //     status =
+            //         sl_Send(newsock, gMetadataResponse,
+            //                 strlen((const char *)gMetadataResponse), 0);
+            //     if((status == SL_ERROR_BSD_EAGAIN) && nonBlocking)
+            //     {
+            //         INFO_PRINT("[ota report task] pending metadata send \n\r");
+            //         usleep(OTA_NB_TIMEOUT * 1000);
+            //         continue;
+            //     }
+            //     else if(status < 0)
+            //     {
+            //         UART_PRINT(
+            //             "[ota report task] Error sending progress metadata to "
+            //             "client, error %d \n\r",
+            //             status);
+            //         sl_Close(newsock);
+            //         goto ota_task_accept_start;
+            //     }
+            //     else
+            //     {
+            //        INFO_PRINT("[ota report task] sent metadata to client \n\r");
+            //         break;
+            //     }
+            // }
+            // while(1)
+            // {
+            //     status = sl_Send(newsock, gPayloadData, contentLen, 0);
+            //     if((status == SL_ERROR_BSD_EAGAIN) && nonBlocking)
+            //     {
+            //         INFO_PRINT("[ota report task] pending report send \n\r");
+            //         usleep(OTA_NB_TIMEOUT * 1000);
+            //         continue;
+            //     }
+            //     else if(status < 0)
+            //     {
+            //         UART_PRINT(
+            //             "[ota report task] Error sending progress data to "
+            //             "client, error %d \n\r",
+            //             status);
+            //         sl_Close(newsock);
+            //         goto ota_task_accept_start;
+            //     }
+            //     else
+            //     {
+            //         INFO_PRINT("[ota report task] sent payload to client \n\r");
+            //         break;
+            //     }
+            // }
 
-            sl_Close(newsock);
+            // sl_Close(newsock);
             sl_Close(rs485NewSock);  // +++++ Close RS485 socket +++++
         }
 
@@ -500,4 +500,5 @@ ota_task_end:
 
         sem_post(&LinkLocal_ControlBlock.otaReportServerStopSignal);
     }
-}
+
+
